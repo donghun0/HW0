b6238c102f9ca646ff3bf8ddc6f199f1212fbe6c
MediaLab <MediaLab@DESKTOP-4LO5JSR>
2023-09-15 10:40:04 +0900

    practice

--- Hello/src/CircleArea.java
+++ Hello/src/CircleArea.java
@@ -2,6 +2,7 @@
 public class CircleArea {
 
 	public static void main(String[] args) {
+		
 		final double PI = 3.14;		// 원주율을 상수로 선언
 		
 		double radius = 10.0;		// 원의 반지름
--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -9,12 +9,14 @@ public class Hello {
 	
 	// starting from main() method
 	public static void main(String[] args) {
+		
 		int i = 20;
 		int s;
 		char a;
 		
 		s = sum(i, 10);		// call sum() method
 		a = '?';
+		
 		System.out.println(a);				// print out character '?'
 		System.out.println("Hello");		// print out string "Hello"
 		System.out.println(s);				// print out int value s
