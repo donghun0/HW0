87b9740b4baff66ea731669047322fe8563f7739
MediaLab <MediaLab@DESKTOP-4LO5JSR>
2023-09-15 10:17:33 +0900

    Practice 2

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,8 +1,22 @@
 
+/*
+ * Hello.java
+ */
 public class Hello {
+	public static int sum(int n, int m) {
+		return n + m;
+	}
+	
+	// starting from main() method
 	public static void main(String[] args) {
-		System.out.println("Hello, World! ");
-		System.out.print("Hello, World! ");
-		System.out.println(" Difference between println and print ");
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i, 10);		// call sum() method
+		a = '?';
+		System.out.println(a);				// print out character '?'
+		System.out.println("Hello");		// print out string "Hello"
+		System.out.println(s);				// print out int value s
 	}
 }
